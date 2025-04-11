8eb8e474e3c57b38270d2975c00801ee21f5a6d9
Dika Dwi Muharamsyah <dsappang@gmail.com>
2025-04-11 16:03:44 +0700

    Add Dashboard controllers for Admin, Operator, and Teacher with basic structure

---
+++ app/Http/Controllers/Teacher/DashboardTeacherController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Teacher;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardTeacherController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
